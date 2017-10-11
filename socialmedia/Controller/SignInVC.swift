b5eab8605b118195de4b77a3a25298003840a0b3
rossthetruth22 <31022402+rossthetruth22@users.noreply.github.com>
2017-10-11 19:47:41 -0400

    Initial commit

---
+++ socialmedia/Controller/SignInVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  socialmedia
+//
+//  Created by Royce Reynolds on 10/11/17.
+//  Copyright © 2017 Royce Reynolds. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
