008537eece5b787e785fb3900ae5bb1a07a29e07
rossthetruth22 <31022402+rossthetruth22@users.noreply.github.com>
2017-10-17 00:01:16 -0400

    Fancy UI
    Making the login UI look a little more better

---
+++ socialmedia/Util/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  socialmedia
+//
+//  Created by Royce Reynolds on 10/16/17.
+//  Copyright © 2017 Royce Reynolds. All rights reserved.
+//
+
+import UIKit
+
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
