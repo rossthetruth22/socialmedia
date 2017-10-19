2c70e5f375a4ebeadde149518bb15ebe8731eb09
rossthetruth22 <31022402+rossthetruth22@users.noreply.github.com>
2017-10-18 21:28:21 -0400

    Password Auth
    Add Firebase email/pass auth

--- socialmedia/Controller/SignInVC.swift
+++ socialmedia/Controller/SignInVC.swift
@@ -13,6 +13,9 @@ import Firebase
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var emailField: FancyField!
+    @IBOutlet weak var passwordField: FancyField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -46,5 +49,28 @@ class SignInVC: UIViewController {
         }
         
     }
+    
+    @IBAction func signInTapped(_ sender: Any) {
+        
+        if let email = emailField.text, let pwd = passwordField.text {
+            Auth.auth().signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil {
+                    print("JESS: Email user authenticated with Firebase")
+                } else{
+                    Auth.auth().createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("JESS: Unable to authenticate with Firebase using email")
+                            print(String(describing: error))
+                        }else{
+                            print("JESS: Successfully authenticated email with Firebase")
+                        }
+                    })
+                }
+            })
+        }
+        
+        
+    }
+    
 }
 
